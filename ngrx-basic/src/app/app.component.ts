f1f0a86b698d751c058868478897886d59a8749a
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-04-19 17:57:33 -0300

    Contruindo seletores

--- ngrx-basic/src/app/app.component.ts
+++ ngrx-basic/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Person } from './models/person.model';
-import { AppState } from './store';
+import { AppState, selectPersonLast, selectPersonCount, selectPerson, selectPersonCountAge } from './store';
 import { PersonAll, PersonDelete, PersonNew, PersonUpdate } from './store/person.actions';
 import { faker } from '@faker-js/faker';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,13 +14,21 @@ import { v4 as uuidv4 } from 'uuid';
 })
 export class AppComponent implements OnInit {
     person$!: Observable<Person[]>;
+    personCount$!: Observable<number>;
+    personLast$!: Observable<any>;
+    personCountAge$!: Observable<any>;
 
     constructor(private store: Store<AppState>) { }
 
     ngOnInit(): void {
         this.store.dispatch(new PersonAll());
-        this.person$ = this.store.select('person');
+        // this.person$ = this.store.select('person');
 
+        // Utilizando seletor criado em index
+        this.person$ = this.store.select(selectPerson);
+        this.personCount$ = this.store.select(selectPersonCount);
+        this.personLast$ = this.store.select(selectPersonLast);
+        this.personCountAge$ = this.store.select(selectPersonCountAge);
     }
 
     addNew() {
@@ -51,8 +59,8 @@ export class AppComponent implements OnInit {
         this.store.dispatch(new PersonUpdate({ person }));
     }
 
-    delete(person: Person) {
-        this.store.dispatch(new PersonDelete({ id: person._id as string }));
+    delete(p: Person) {
+        this.store.dispatch(new PersonDelete({ id: p._id as string }));
     }
 
 }
--- ngrx-basic/src/app/store/index.ts
+++ ngrx-basic/src/app/store/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap } from "@ngrx/store";
+import { ActionReducerMap, createSelector } from "@ngrx/store";
 import { Person } from "../models/person.model";
 import * as fromPersonReducer from './person.reducer';
 
@@ -9,3 +9,27 @@ export interface AppState {
 export const appReducers: ActionReducerMap<AppState> = {
     person: fromPersonReducer.reducer
 };
+
+
+// Exemplo de criação de seletor
+export const selectPerson = (state: AppState) => state.person;
+
+export const selectPersonCount = createSelector(
+    selectPerson,
+    (person) => person.length
+);
+
+export const selectPersonLast = createSelector(
+    selectPerson,
+    selectPersonCount,
+    (persons, n) => persons[--n]
+);
+
+export const selectPersonCountAge = createSelector(
+    selectPerson,
+    (persons) => {
+        let countAge = 0;
+        persons.forEach((p) => countAge += p.age);
+        return countAge;
+    }
+);
